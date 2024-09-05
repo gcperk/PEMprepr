8125b05850a597cabf36f97124a921188685765c
Andy Teucher <andy.teucher@gmail.com>
2024-09-05 16:46:59 -0700

    More tests

--- tests/testthat/test-snap_aoi.R
+++ tests/testthat/test-snap_aoi.R
@@ -1,7 +1,6 @@
 test_that("snap_aoi works with sf obj and file", {
   snap1 <- snap_aoi(
-    sf::st_read(
-      fs::path_package("PEMprepr", "extdata/datecreek_aoi.gpkg")), 
+    sf::st_read(fs::path_package("PEMprepr", "extdata/datecreek_aoi.gpkg")), 
     write_output = FALSE
   )
 
@@ -12,3 +11,56 @@ test_that("snap_aoi works with sf obj and file", {
 
   expect_equal(snap1, snap2)
 })
+
+test_that("snap_aoi writing works", {
+  dir <- withr::local_tempdir()
+
+  snap_aoi(
+    fs::path_package("PEMprepr", "extdata/datecreek_aoi.gpkg"),
+    out_dir = dir
+  )
+
+  expect_true(file.exists(fs::path(dir, "aoi_snapped.gpkg")))
+
+})
+
+test_that("snap_aoi with 'method' works", {
+  snap_expand <- snap_aoi(
+    fs::path_package("PEMprepr", "extdata/datecreek_aoi.gpkg"),
+    write_output = FALSE
+  )
+
+  snap_shrink <- snap_aoi(
+    fs::path_package("PEMprepr", "extdata/datecreek_aoi.gpkg"), 
+    method = "shrink",
+    write_output = FALSE
+  )
+
+  expect_gt(
+    sf::st_area(snap_expand), 
+    sf::st_area(snap_shrink)
+  )
+
+})
+
+test_that("snap_aoi buffer works", {
+  
+  snap_0 <- snap_aoi(
+    fs::path_package("PEMprepr", "extdata/datecreek_aoi.gpkg"),
+    write_output = FALSE
+  )
+
+  snap_100 <- snap_aoi(
+    fs::path_package("PEMprepr", "extdata/datecreek_aoi.gpkg"), 
+    buffer = 100,
+    write_output = FALSE
+  )
+
+  expect_gt(
+    sf::st_area(snap_100),
+    sf::st_area(snap_0)
+  )
+})
+
+# TODO: Add test that creates PEMr project and test searching for AOI in folder 
+# structure
