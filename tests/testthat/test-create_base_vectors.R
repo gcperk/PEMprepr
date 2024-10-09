f7f5896bb92cc879677da332c955f5b0752225de
Andy Teucher <andy.teucher@gmail.com>
2024-10-09 14:16:40 -0700

    add test for create_base_vectors with file

--- tests/testthat/test-create_base_vectors.R
+++ tests/testthat/test-create_base_vectors.R
@@ -21,6 +21,22 @@ test_that("create_base_vectors works with sf object", {
   )
 
   expect_equal(outdir, out)
+  expect_snapshot(fs::path_file(fs::dir_ls(outdir)))
+})
 
+test_that("create_base_vectors works with file", {
+  outdir <- withr::local_tempdir()
+
+  aoi_snapped <- snap_aoi(
+    fs::path_package("PEMprepr", "extdata/datecreek_aoi.gpkg"),
+    out_dir = fs::path(outdir, "snap")
+  )
+
+  out <- create_base_vectors(
+    fs::path(outdir, "snap", "aoi_snapped.gpkg"),
+    out_dir = outdir
+  )
+
+  expect_equal(outdir, out)
   expect_snapshot(fs::path_file(fs::dir_ls(outdir)))
 })
