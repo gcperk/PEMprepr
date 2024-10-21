82a071edb4bccb9efcdd63a7209f0dcae27f656b
Andy Teucher <andy.teucher@gmail.com>
2024-10-21 11:29:15 -0700

    delete bcmaps cache on testthat teardown

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,4 +1,6 @@
 # Set a directory for bcmaps cded cache so they can be reused across tests
-opts <- options("bcmaps.data_dir" = fs::path(tempdir(), "bcmaps_cache"))
+cache_path <- fs::path(tempdir(), "bcmaps_cache")
+opts <- options("bcmaps.data_dir" = cache_path)
 
 withr::defer(options(opts), teardown_env())
+withr::defer(unlink(cache_path, recursive = TRUE), teardown_env())
