7efb4ecaeb08c8e73a84ea020966b4a5d59845d2
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-10-22 16:06:36 -0700

    add bec helper

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -4,3 +4,20 @@ make_test_aoi <- function(outdir) {
     out_dir = fs::path(outdir, "snap")
   )
 }
+
+
+make_test_bec <- function(outdir) {
+
+  aoi_snapped <- make_test_aoi(outdir)
+  #out_dir = fs::path(outdir, "snap")
+
+  bec <- bcdata::bcdc_query_geodata("f358a53b-ffde-4830-a325-a5a03ff672c3") |>
+      bcdata::filter(bcdata::INTERSECTS(aoi_snapped)) |>
+      bcdata::select("MAP_LABEL") |>
+      bcdata::collect() |>
+      dplyr::select("MAP_LABEL")
+
+  bec <- sf::st_intersection(bec, aoi_snapped)
+  sf::st_write(bec, fs::path(outdir, "bec.gpkg"), append = FALSE)
+}
+
