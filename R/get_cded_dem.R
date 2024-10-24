e4ebb728eaa23e91e10c51ca88dcacb159d97f2b
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2024-10-24 15:21:23 -0700

    update to allow res to override template raster

--- R/get_cded_dem.R
+++ R/get_cded_dem.R
@@ -4,11 +4,12 @@
 #' Canadian Digital Elevation Data set to match the raster template built using
 #' create_raster_template().
 #'
-#' @param  aoi the `SpatRast` created using the create_base_raster() on which the
-#'      DEM data will be extracted, currently accepts raster (.tif) or filepath.
-#' @param res resolution in meters of final project
-#' @param write_output should the snapped aoi bounding box be written to disk?
-#'     If `TRUE` (default), will write to `out_dir`
+#' @param  aoi An `SpatRast` object or path to a spatial file (.tif) usually generated
+#'      when calling [create_base_raster()]. Should be a meter based coordinate reference system.
+#' @param res resolution in meters of final project. If no values specified (default is FALSE),
+#'      output spatRast will match the resolution of aoi.
+#' @param write_output should the cded spatRast be written to disk?
+#'     If `TRUE` (default), will write to `out_dir` in a subfolder defined by resolution e.g: "25m".
 #' @param out_dir  the root directory to hold the cded dem spatRast file. If not
 #'     specified uses the default from the `fid` folder structure.
 #' @inheritParams terra::writeRaster
@@ -19,18 +20,23 @@
 #'
 #' @examples
 #' \dontrun{
-#' get_cded_dem(aoi_bb = file.path(fid$shape_dir_1010[2],"aoi_snapped.gpkg"),
-#'     res = 5, out_dir = fid$cov_dir_1020[2]))
+#' get_cded_dem(
+#'   aoi = fs::path(PEMprepr::read_fid()$dir_1020_covariates$path_abs, "25m", "template.tif"),
+#'   res = FALSE,
+#'   out_dir = PEMprepr::read_fid()$dir_1020_covariates$path_abs,
+#'   write_output = TRUE
+#'   overwrite = FALSE)
 #' }
 get_cded_dem <- function(aoi = fs::path(PEMprepr::read_fid()$dir_1020_covariates$path_abs, "25m", "template.tif"),
                          res = FALSE,
                          out_dir = PEMprepr::read_fid()$dir_1020_covariates$path_abs,
                          write_output = TRUE,
                          overwrite = FALSE, ...) {
+
   if (inherits(aoi, c("character"))) {
     aoi <- terra::rast(aoi)
   } else if (!inherits(aoi, c("SpatRaster"))) {
-    cli::cli_abort("{.var aoi} must be an spatRast or a path to a .tif file")
+    cli::cli_abort("{.var aoi} must be a SpatRaster or a path to a file")
   }
 
   aoi_res <- terra::res(aoi)[1]
@@ -40,34 +46,44 @@ get_cded_dem <- function(aoi = fs::path(PEMprepr::read_fid()$dir_1020_covariates
       cli::cli_abort("{.var res} must be numeric")
     }
 
+    if(aoi_res != res) {
+
+      cli::cli_alert_warning(
+        "Updating cded to resolution specified"
+      )
+
+      aoi_out <- aoi
+      res(aoi_out) = res
+      aoi_template <- resample(aoi, aoi_out)
+
+    } else {
+
+      aoi_template <- aoi
+      cli::cli_alert_warning(
+        "Specified res matches template resolution, output cded resolution will be {.var res}"
+      )
+    }
+
     output_res <- res
+
   } else {
+
+    aoi_template <- aoi
     output_res <- aoi_res
     cli::cli_alert_warning(
-      "No spatial resolution specified, cded output resolution will match input raster"
+      "No spatial resolution specified, cded output spatial resolution will match input raster"
     )
   }
 
-  output_dir <- fs::path(out_dir, paste0(res, "m"))
+  cded_raw <- bcmaps::cded_terra(aoi_template, ...)
+  #cded_raw <- bcmaps::cded_terra(aoi_template)#, ...)
 
-  cded_raw <- bcmaps::cded_terra(aoi, ...)
+  cded <- terra::project(cded_raw, aoi_template)
 
-
-
-
-  aoi_rast <- create_template_raster(aoi, res = res, write_output = FALSE )
-
-  cded <- terra::project(cded_raw, aoi)
-
-  cded_res <- terra::res(cded)[1]
-
-  if(cded_res != output_res) {
-    cded <- terra::disagg(cded, cded_res/output_res)
-  }
-
-
-  ## check if folder exists
   if (write_output) {
+
+    output_dir <- fs::path(out_dir, paste0(output_res, "m"))
+
     if (!fs::dir_exists(fs::path(output_dir))) {
       fs::dir_create(fs::path(output_dir), recurse = TRUE)
     }
